5f4cae0037310a97dbabf9b47cff98d4a775e0c8
Windu Nursetyadi <windudr@gmail.com>
2021-01-14 16:56:32 +0700

    fixing app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://windu-test:test1234@cluster0.5v8hh.mongodb.net/node-auth';
+const dbURI = '';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
